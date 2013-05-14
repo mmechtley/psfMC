Output residual and model files. Fixed shift bug in convolution. Replaced M_rot_inv with M_rot.T, since it's a rotation matrix.

psfMC/fitting.py
@@ -6,12 +6,15 @@ from .models import multicomponent_model
 
 # TODO: Better interface for supplying components. File? As param?
 def model_psf_mcmc(subfile, subIVMfile, psffile, psfIVMfile,
-                    maskRegionFile=None, outfile=None, magZP=0, **kwargs):
+                    mask_file=None, outfile=None, db_filename=None,
+                    magZP=0, **kwargs):
     # TODO: Don't hardcode these!
     components = [('psf', 60, 70, 60, 70, 18, 23),
                   ('sersic', 60, 70, 60, 70, 22, 26,
                    1.5, 8.0, 0.5, 8.0, 0.1, 1.0, 0, 360)]
 
+    if db_filename is None:
+        db_filename = 'psffit'
     if outfile is None:
         outfile = subfile.replace('sci', 'resid')
     subData = pyfits.getdata(subfile, ignore_missing_end=True)
@@ -19,20 +22,17 @@ def model_psf_mcmc(subfile, subIVMfile, psffile, psfIVMfile,
     psfData = pyfits.getdata(psffile, ignore_missing_end=True)
     psfDataIVM = pyfits.getdata(psfIVMfile, ignore_missing_end=True)
 
-    if maskRegionFile is not None:
+    if mask_file is not None:
         import pyregion
         hdr = pyfits.getheader(subfile)
-        regfilt = pyregion.open(maskRegionFile).as_imagecoord(hdr).get_filter()
+        regfilt = pyregion.open(mask_file).as_imagecoord(hdr).get_filter()
         mask = regfilt.mask(subData.shape)
         subData = np.ma.masked_array(subData, mask=~mask)
         subDataIVM[~mask] = 0
-        maskInd = np.where(mask)
-        fitslice = (slice(maskInd[0].min(), maskInd[0].max() + 1),
-                    slice(maskInd[1].min(), maskInd[1].max() + 1))
-        subData = subData[fitslice]
-        subDataIVM = subDataIVM[fitslice]
+        # TODO: Use slice to fit only the masked area. But messes up xy pos.
 
     # Normalize the PSF kernel
+    # TODO: Convert to float64 first?
     psfScale = 1 / psfData.sum()
     psfData *= psfScale
     psfDataIVM /= psfScale**2
@@ -41,10 +41,10 @@ def model_psf_mcmc(subfile, subIVMfile, psffile, psfIVMfile,
                                         psfData, psfDataIVM,
                                         components=components,
                                         magZP=magZP),
-                   db='pickle')
+                   db='pickle', name=db_filename)
     # TODO: Set these based on total number of unknown components
-    kwargs.setdefault('iter', 2500)
-    kwargs.setdefault('burn', 1800)
+    kwargs.setdefault('iter', 6000)
+    kwargs.setdefault('burn', 3000)
     kwargs.setdefault('tune_interval', 25)
     kwargs.setdefault('thin', 5)
     sampler.sample(**kwargs)
@@ -52,30 +52,24 @@ def model_psf_mcmc(subfile, subIVMfile, psffile, psfIVMfile,
     ## Saves out to pickle file
     sampler.db.close()
 
+    # TODO: Remove debug output
     stats = sampler.stats()
-    for stoch in stats:
+    for stoch in sorted(stats):
         if stoch.startswith(tuple(str(i) for i in range(len(components)))):
-            print '{}: mean: {} std: {}'.format(stoch, stats[stoch]['mean'],
-                                            stats[stoch]['standard deviation'])
+            print '{}: mean: {} std: {} final: {}'.format(
+                stoch, stats[stoch]['mean'], stats[stoch]['standard deviation'],
+                sampler.db.trace(stoch)[-1])
 
-    # TODO: Write out copies of subtracted data file, and IVM to go with it
+    # TODO: Flag for writing out files, add information to fits headers
 
-    # xyshift = stats['xyshift']['mean']
-    # xyerr = stats['xyshift']['standard deviation']
-    # scale = stats['scale']['mean']
-    # scaleErr = stats['scale']['standard deviation']
-    # sd = pyfits.getdata(subfile)
-    # psfd = pyfits.getdata(psffile)
-    # hdr = pyfits.getheader(subfile)
-    # hdr.add_history('Used PSF {}'.format(psffile))
-    # hdr.add_history('PSF Shift: {} Err: {}'.format(xyshift, xyerr))
-    # hdr.add_history('PSF Scale: {} Err: {}'.format(scale, scaleErr))
+    with pyfits.open(subfile) as f:
+        f[0].data -= sampler.db.trace('convolved_model')[-1]
+        f.writeto(outfile, clobber=True, output_verify='fix')
+        f[0].data = sampler.db.trace('convolved_model')[-1]
+        f.writeto(outfile.replace('resid', 'model'), clobber=True,
+                  output_verify='fix')
 
-    # pyfits.writeto(outfile,
-    #                data=sd - scale * shift(psfd, xyshift, order=1, mode='wrap'),
-    #                header=hdr, clobber=True, output_verify='fix')
-
-    # pyfits.writeto(subfile.replace('sci','residerr'),
-    #                data=shift(psfDataIVM, xyshift, order=1, mode='wrap')/scale,
-    #                header=pyfits.getheader(subIVMfile),
-    #                clobber=True)
+    with pyfits.open(subIVMfile) as f:
+        f[0].data = sampler.db.trace('composite_IVM')[-1]
+        f.writeto(outfile.replace('resid','residerr'), clobber=True,
+                  output_verify='fix')

psfMC/models.py
@@ -38,7 +38,12 @@ def _convolve(img, kernel):
     boundary mode to be wrap.
     TODO: consider zero-padding inputs to make sizes power-of-two
     """
-    return np.fft.irfft2(np.fft.rfft2(img) * np.fft.rfft2(kernel, img.shape))
+    pad = np.asarray(img.shape) - np.asarray(kernel.shape)
+    kernel_pad = np.zeros_like(img)
+    kernel_pad[pad[0]//2:pad[0]//2+pad[0],
+               pad[1]//2:pad[1]//2+pad[1]] = kernel
+    return np.fft.ifftshift(np.fft.irfft2(np.fft.rfft2(img) *
+                                          np.fft.rfft2(kernel_pad)))
 
 
 def _add_sersic(arr, magZP, xy, mag, reff, index, axis_ratio, angle):
@@ -53,8 +58,7 @@ def _add_sersic(arr, magZP, xy, mag, reff, index, axis_ratio, angle):
     sin_ang, cos_ang = np.sin(angle), np.cos(angle)
     M_inv_scale = np.diag((1/reff, 1/(reff*axis_ratio))) ** 2
     M_rot = np.asarray((cos_ang, -sin_ang, sin_ang, cos_ang)).reshape(2, 2)
-    M_inv_rot = np.asarray((cos_ang, sin_ang, -sin_ang, cos_ang)).reshape(2, 2)
-    M_inv_xform = np.dot(np.dot(M_rot, M_inv_scale), M_inv_rot)
+    M_inv_xform = np.dot(np.dot(M_rot, M_inv_scale), M_rot.T)
 
     coords = [np.arange(arr.size) % arr.shape[1],
               np.arange(arr.size) // arr.shape[1]]
@@ -91,7 +95,6 @@ def multicomponent_model(subData, subDataIVM, psf, psfIVM,
      ('sersic', 120, 136, 120, 136, 21, 28, 1.5, 3.5, 0.5, 8, 0.1, 1.0, 0, 360)]
     """
     model_comps = []
-    modelpx = np.zeros_like(subData)
 
     for count, component in enumerate(components):
         name = component[0]
@@ -133,8 +136,7 @@ def multicomponent_model(subData, subDataIVM, psf, psfIVM,
     def raw_model(model_comps=model_comps):
         _debug_timer('start')
         # TODO: shouldn't have to make this writeable every time
-        modelpx.flags.writeable = True
-        modelpx[:,:] = 0
+        modelpx = np.zeros_like(subData)
         for comp in model_comps:
             if comp[0] == 'psf':
                 _add_point_source(modelpx, magZP, *comp[1:])