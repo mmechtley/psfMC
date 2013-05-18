acb799bb831dc46f3a5d09c55194840b547e66fd
Matt Mechtley <mmechtley@gmail.com>
2013-05-17 18:29:20 -0700

    Adjusted default parameters in example file.

--- z6_quasar.py
+++ z6_quasar.py
@@ -10,13 +10,13 @@ except ImportError:
 
 magzp = {'F125W':26.2303, 'F160W':25.9463}
 
-obsfiles = ['examples/sci_SDSSJ081518.99+103711.5.fits']
-psffiles = ['examples/sci_psf16.fits']
+obsfiles = ['examples/sci_J0005-0006.fits']
+psffiles = ['examples/sci_psf.fits']
 
 fit_components = [('sky', -1e-1, 1e-1),
-                  ('psf', 120, 140, 120, 140, 15, 20),
-                  ('sersic', 120, 140, 120, 140, 16, 27.5,
-                   1.5, 8.0, 0.5, 8.0, 0.1, 1.0, 0, 360)]
+                  ('psf', 60, 70, 60, 70, 18, 24),
+                  ('sersic', 60, 70, 60, 70, 20, 27.5,
+                   0.5, 8.0, 0.5, 8.0, 0.1, 1.0, 0, 360)]
 
 for obsfile, psffile in zip(obsfiles, psffiles):
     obsIVMfile = obsfile.replace('sci', 'ivm')
@@ -26,11 +26,11 @@ for obsfile, psffile in zip(obsfiles, psffiles):
     model_galaxy_mcmc(obsfile, obsIVMfile, psffile, psfIVMfile,
                       fit_components=fit_components,
                       output_name=output_name, mag_zeropoint=magzp[filter],
-                      burn=500, iter=1000)
+                      burn=5000, iter=10000)
 
     db = pymc.database.pickle.load(output_name+'_db.pickle')
-    for trace_name in ('1_psf_mag', '2_sersic_mag', '2_sersic_re', '2_sersic_n',
-                       '2_sersic_angle', '2_sersic_axisratio'):
+    for trace_name in ('0_sky_adu', '1_psf_mag', '2_sersic_mag', '2_sersic_re',
+                       '2_sersic_n', '2_sersic_angle', '2_sersic_axisratio'):
         if pp is not None:
             pp.hist(db.trace(trace_name)[:], bins=20)
             pp.title(trace_name)
