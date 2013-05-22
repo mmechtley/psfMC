e6148f71ace351abcfcc1389a6f0947c6b9ca7f8
Matt Mechtley <mmechtley@gmail.com>
2013-05-21 23:24:55 -0700

    ModelComponents package added to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from numpy.distutils.core import setup
 
 setup(name='psfMC',
       version='0.1',
-      packages=['psfMC'],
+      packages=['psfMC', 'psfMC.ModelComponents'],
       requires=['numpy', 'pymc', 'scipy', 'pyfits'])
