6992906ac6204cca2eb89238ec231b7b2baab405
Matt Mechtley <mmechtley@gmail.com>
2013-11-15 15:54:26 -0700

    FIxed import of pymc distributions

--- psfMC/distributions.py
+++ psfMC/distributions.py
@@ -3,10 +3,13 @@ import pymc.distributions
 from pymc.Container import Container
 from pymc import CircularStochastic
 
-# Magically import all pymc distributions, without requiring name argument
-def _magic_dist(className):
+
+def _magic_dist(class_name):
+    """
+    Magically import all pymc distributions, without requiring name argument
+    """
     return lambda *args, **kwargs: \
-        pymc.distributions.__dict__[className]('', *args, **kwargs)
+        pymc.distributions.__dict__[class_name]('', *args, **kwargs)
 
 _dists = [pymc.distributions.capitalize(_dist) for _dist
           in pymc.distributions.availabledistributions]
@@ -15,8 +18,8 @@ for _dist in _dists:
 
 
 class CircUniform(CircularStochastic, pymc.distributions.Uniform):
-     def __init__(self, lower, upper, *args, **kwargs):
-         self.interval_parents = Container([upper, lower])
-         pymc.distributions.Uniform.__init__(self, name='',
-                                             lower=lower, upper=upper,
-                                             *args, **kwargs)
+    def __init__(self, lower, upper, *args, **kwargs):
+        self.interval_parents = Container([upper, lower])
+        pymc.distributions.Uniform.__init__(self, name='',
+                                            lower=lower, upper=upper,
+                                            *args, **kwargs)
