a4f2cdd64ae8ab6b728b08308c8aab17ce7616c5
Matt Mechtley <mmechtley@gmail.com>
2013-11-15 23:40:45 -0700

    Added Schechter distribution

--- psfMC/distributions.py
+++ psfMC/distributions.py
@@ -1,6 +1,9 @@
 import pymc.distributions
 from pymc.Container import Container
-from pymc import CircularStochastic
+from pymc import CircularStochastic, stochastic_from_dist
+from numpy import log, log10
+from numpy.random import random
+from scipy.special import gammaincinv
 
 
 def _magic_dist(class_name):
@@ -22,3 +25,22 @@ class CircUniform(CircularStochastic, pymc.distributions.Uniform):
         pymc.distributions.Uniform.__init__(self, name='',
                                             lower=lower, upper=upper,
                                             *args, **kwargs)
+
+
+def _schechter_like(x, alpha, mstar):
+    lratio = 10**(-0.4*(x-mstar))
+    return (alpha+1)*log(lratio) - lratio  # + log(0.4*log(10)*phst)
+
+
+def _rschechter(alpha, mstar, size=None):
+    """
+    Schechter random variable. Schechter CDF is:
+    N(>L) = nstar * gammainc(a+2, lratio)
+    http://www.astro.virginia.edu/class/whittle/astr553/Topic04/Lecture_4.html
+    where nstar = 0.4*log(10)*phst and +2 is because of +1 for mags
+    """
+    lratio = gammaincinv(alpha+2, random(size))
+    return -2.5*log10(lratio) + mstar
+
+Schechter = stochastic_from_dist('Schechter', logp=_schechter_like,
+                                 random=_rschechter)
