8ec7115c4044037501e03a2be4d2ff652901c6b4
Matt Mechtley <mmechtley@gmail.com>
2013-11-17 16:46:48 -0700

    Schechter distribution test (currently broken)

--- psfMC/distributions.py
+++ psfMC/distributions.py
@@ -1,9 +1,9 @@
 import pymc.distributions
 from pymc.Container import Container
 from pymc import CircularStochastic, stochastic_from_dist
-from numpy import log, log10
+from numpy import log, log10, exp, inf, sum
 from numpy.random import random
-from scipy.special import gammaincinv
+from scipy.special import gammainccinv
 
 
 def _magic_dist(class_name):
@@ -27,20 +27,36 @@ class CircUniform(CircularStochastic, pymc.distributions.Uniform):
                                             *args, **kwargs)
 
 
-def _schechter_like(x, alpha, mstar):
-    lratio = 10**(-0.4*(x-mstar))
-    return (alpha+1)*log(lratio) - lratio  # + log(0.4*log(10)*phst)
+def _schechter_like(x, alpha=-1.0, mstar=0.0, faint_cutoff=None):
+    """
+    Schechter (gamma) log-likelihood. Parameterized in magnitudes, ie.
+    N(M) dM \propto Lrat^(alpha+1) * exp(-Lrat) dM
+    where Lrat = 10^(-0.4*(M - mstar))
+    """
+    if faint_cutoff is None:
+        faint_cutoff = mstar + 20
+    lum_ratio = 10**(-0.4*(x-mstar))
+    #faint_ratio = 10**(-0.4*(faint_cutoff - mstar))
+    #tot_count, tot_err = _schechter_int(alpha, faint_ratio, inf)
+    return sum((alpha+1)*log(lum_ratio) - lum_ratio)  #- log(tot_count)
+    # + log(0.4*log(10)*phst)
 
 
-def _rschechter(alpha, mstar, size=None):
+def _rschechter(alpha=-1.0, mstar=0.0, faint_cutoff=None, size=None):
     """
     Schechter random variable. Schechter CDF is:
     N(>L) = nstar * gammainc(a+2, lratio)
     http://www.astro.virginia.edu/class/whittle/astr553/Topic04/Lecture_4.html
     where nstar = 0.4*log(10)*phst and +2 is because of +1 for mags
     """
-    lratio = gammaincinv(alpha+2, random(size))
+    if faint_cutoff is None:
+        faint_cutoff = mstar + 20
+    # FIXME: Alpha +1 seems to match the pdf from above. Unsure what's up
+    y_gamma = random(size)
+    #y_gamma *= gammaincc(a+2,faint_cutoff)
+    lratio = gammainccinv(alpha+2, y_gamma)
     return -2.5*log10(lratio) + mstar
+
 
 Schechter = stochastic_from_dist('Schechter', logp=_schechter_like,
                                  random=_rschechter)
