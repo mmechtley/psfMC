6cf1416e3065fb41a8b7d310c39abe965c77886a
mmechtley <mmechtley@gmail.com>
2014-03-10 17:15:36 +0100

    More version cleanup. Canonical (release) version now stored in __init__.py. Patch versions (individual git commits) are included in the egg info if .git dir is present and git is installed.

--- psfMC/__init__.py
+++ psfMC/__init__.py
@@ -1,5 +1,5 @@
+__version__ = 'v1.0a1'
+
 from .fitting import model_galaxy_mcmc, save_posterior_model
 from .models import multicomponent_model
 from .analysis import potential_scale_reduction, num_effective_samples
-
-__version__ = 'v1.0a1'
--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
+import os
+import re
+from warnings import warn
+from subprocess import check_output, CalledProcessError
 from numpy.distutils.core import setup
 
-setup(name='psfMC',
-      version='v1.0a1',
-      packages=['psfMC', 'psfMC.ModelComponents'],
-      requires=['numpy', 'pymc', 'scipy', 'pyfits'])
+_packages = ['psfMC', 'psfMC.ModelComponents']
+_requires = ['numpy', 'pymc', 'scipy', 'pyfits']
+
+
+def _version_from_init():
+    main_init = os.path.join(_packages[0], '__init__.py')
+    ver_pat = re.compile('__version__\s*=\s*\'([^\']*)\'')
+    with open(main_init) as initfile:
+        for line in initfile.readlines():
+            match = ver_pat.match(line)
+            if match is not None:
+                return match.group(1)
+    return ''
+
+_version = _version_from_init()
+
+if os.path.exists('.git'):
+    try:
+        _version = check_output(['git', 'describe'])
+    except CalledProcessError:
+        warn('git versioning information found but call to git describe '
+             'failed. Version number will not include commit number and hash '
+             'for development builds.')
+
+setup(name=_packages[0],
+      version=_version,
+      author='Matt Mechtley',
+      author_email='matt@cherem.org',
+      url='https://github.com/mmechtley/psfMC',
+      license='BSD 2-clause',
+      description='Markov Chain Monte Carlo modeling of 2-dimensional surface '
+                  'brightness distributions of quasar host galaxies',
+      packages=_packages,
+      requires=_requires)
